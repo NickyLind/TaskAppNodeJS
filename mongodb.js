b9980458101d64d513ba3eff06a6480b2eb83d8e
Nick Lindau <nicholaithegreat@gmail.com>
2021-10-05 19:04:51 -0700

    add example functions for updating documents/fields

--- mongodb.js
+++ mongodb.js
@@ -4,10 +4,6 @@ const { MongoClient, ObjectID } = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-// const id = new ObjectID()
-// console.log(id.id.length);
-// console.log(id.toHexString().length);
-
 MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
   if(error) {
     return console.log(`Unable to connect to database: ${error}`);
@@ -15,34 +11,27 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
   const db = client.db(databaseName);
   
-  // db.collection('users').findOne({ _id: new ObjectID("6159242c6f024e66ec23b0af")}, (error, user) => {
-  //   if (error) return console.log("Unable to fetch");
+  // db.collection('users').updateOne({ 
+  //   _id: new ObjectID('6159242c6f024e66ec23b0af')
+  // }, {
+  //   $inc: {
+  //     age: 40
+  //   }
+  // }).then((result) => {
+  //   console.log(result);
+  // }).catch((error) => {
+  //   console.log(error);
+  // })
 
-  //   console.log(user);
-  // });
-  // //? findOne takes in 2 required arguments: The first is an object and the second is a callback function
-
-  // db.collection('users').find({ age: 30 }).toArray((error, users) => {
-  //   if (error) return console.log('an error occured');
-
-  //   console.log(users);
-  // });
-
-  // db.collection('users').find({ age: 30 }).count((error, count) => {
-  //   if (error) return console.log('an error has occured');
-
-  //   console.log(count);
-  // });
-
-  db.collection('tasks').findOne( { _id: new ObjectID("6159ddbd99981b5728b3b222")}, (error, document) => {
-    if (error) return console.log('There was an issue.');
-
-    console.log(document);
-  });
-
-  db.collection('tasks').find({ completed: false}).toArray((error, documents) => {
-    if (error) return console.log('An error occured');
-
-    console.log(documents);
+  db.collection('tasks').updateMany({
+    completed: false
+  }, {
+    $set: {
+      completed: true
+    }
+  }).then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    console.log(error);
   });
 });
