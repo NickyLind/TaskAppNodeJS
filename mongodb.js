298a7596fddf01494809fb465a2fca17ac0ae31b
Nick Lindau <nicholaithegreat@gmail.com>
2021-10-05 19:35:46 -0700

    create file for connecting mongoose

--- mongodb.js
+++ mongodb.js
@@ -11,19 +11,5 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
   const db = client.db(databaseName);
   
-  // db.collection('users').deleteMany({
-  //   age: 70
-  // }).then((result) => {
-  //   console.log(result);
-  // }).catch((error) => {
-  //   console.log(error);
-  // })  
-
-  db.collection('tasks').deleteOne({
-    description: 'put shoes on the right feet'
-  }).then((result) => {
-    console.log(result);
-  }).catch((error) => {
-    console.log(error);
-  })
+  
 });
