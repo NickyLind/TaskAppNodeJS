f5556bde41df263b00936ac9ef9b2265bf3708a6
Nick Lindau <nicholaithegreat@gmail.com>
2021-10-05 17:46:31 -0700

    create test functions for reading documents by id and fields

--- mongodb.js
+++ mongodb.js
@@ -13,52 +13,36 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
     return console.log(`Unable to connect to database: ${error}`);
   }
 
-  // console.log(`Connected correctly!`);
   const db = client.db(databaseName);
-  //? takes in the name of the database we are trying to manipulate, and returns a database reference that we save in a variable called db
-  // db.collection('users').insertOne({
-  //   _id: id,
-  //   name: "Stefani",
-  //   age: 31
-  // }, (error, result) => {
-  //   if(error) {
-  //     return console.log(`We ran into a fucking problem buddy: ${error}`);
-  //   }
+  
+  // db.collection('users').findOne({ _id: new ObjectID("6159242c6f024e66ec23b0af")}, (error, user) => {
+  //   if (error) return console.log("Unable to fetch");
 
-  //   console.log(result.ops);
+  //   console.log(user);
   // });
-  //? we call the connect method on the MongoClient in order to set up the connection to our database. Connect takes a few different arguments, the first is the connectionURL. The second argument is an options object(the option we pass in is because the old parser used to parse the url is depricated, so we want to use the new one). The final argument is a callback function that is called when we get connected to the database (async operation). The callback function 
+  // //? findOne takes in 2 required arguments: The first is an object and the second is a callback function
 
-  // db.collection('users').insertMany([
-  //   {
-  //     name: 'Jen',
-  //     age: 28
-  //   }, {
-  //     name: 'Gunther',
-  //     age: 27
-  //   }
-  // ], (error, result) => {
-  //   if(error) {
-  //     return console.log(`something fucking happened bro: ${error}`);
-  //   }
-  //   console.log(result.ops);
-  // })
+  // db.collection('users').find({ age: 30 }).toArray((error, users) => {
+  //   if (error) return console.log('an error occured');
 
-  // db.collection('tasks').insertMany([
-  //   {
-  //     description: 'wake up alive',
-  //     completed: true
-  //   }, {
-  //     description: 'try not to drown in the shower',
-  //     completed: false
-  //   }, {
-  //     description: 'put shoes on the right feet',
-  //     completed: false
-  //   }
-  // ], (error, result) => {
-  //   if(error) return console.log(`There was an error homie: ${error}`);
-  //   console.log(result.ops);
-  // })
+  //   console.log(users);
+  // });
 
+  // db.collection('users').find({ age: 30 }).count((error, count) => {
+  //   if (error) return console.log('an error has occured');
 
+  //   console.log(count);
+  // });
+
+  db.collection('tasks').findOne( { _id: new ObjectID("6159ddbd99981b5728b3b222")}, (error, document) => {
+    if (error) return console.log('There was an issue.');
+
+    console.log(document);
+  });
+
+  db.collection('tasks').find({ completed: false}).toArray((error, documents) => {
+    if (error) return console.log('An error occured');
+
+    console.log(documents);
+  });
 });
